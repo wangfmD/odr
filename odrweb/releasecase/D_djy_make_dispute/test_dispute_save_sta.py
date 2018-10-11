75a604be59918afaa791db5b25821a460a2343dc
王发明 <wangfm@3bu.cn>
2018-10-11 10:45:50 +0800

    优化文件名称

--- odrweb/releasecase/D_djy_make_dispute/test_dispute_save_sta.py
+++ odrweb/releasecase/D_djy_make_dispute/test_dispute_save_sta.py
@@ -13,8 +13,8 @@ sys.setdefaultencoding("utf-8")
 t = 1
 
 
-class OdrJfInput(unittest.TestCase):
-    '''纠纷登记-机构登记员'''
+class DisputeSave(unittest.TestCase):
+    '''机构登记员-纠纷-保存'''
 
     def setUp(self):
         self.homepage = HomePage()
@@ -25,15 +25,16 @@ class OdrJfInput(unittest.TestCase):
         self.homepage.quit()
 
     def test_01(self):
-        '''机构登记员-登记纠纷提交-申自然人-被自然人代理人'''
-        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申自然人-被自然人代理人",
+        '''机构登记员-登记纠纷提交-申自然人-被自然人'''
+        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申自然人-被自然人",
                        "applicant_type": u"自然人",  # 自然人 法人 非法人组织
                        "disputer_type": u"自然人",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
-                       "agent_b_type": "special",  # common special,
+                       "agent_b_type": "",  # common special,
+                       'none_mediator': True,
 
                        "jf_appeal": u"假一赔十",
-                       "applicant_name": u"企业或机构名称",  #
+                       "applicant_name": u"a企业或机构名称",  #
                        "world_credit_id": "abcde1234567890",
                        "applicant": u"钱桂林",
                        "applicant_tel": "13160077223",
@@ -43,7 +44,7 @@ class OdrJfInput(unittest.TestCase):
                        "disputer": u"王发明",
                        "disputer_tel": "13913031374",
                        "disputer_world_credit_id": "zxcvbnmasdfghjk123",
-                       "disputer_name": u"企业或机构名称",
+                       "disputer_name": u"b企业或机构名称",
                        "disputer_id": "",
                        "disputer_addr": u"10栋1单元101",
 
@@ -57,21 +58,21 @@ class OdrJfInput(unittest.TestCase):
                        }
         self.homepage.organization_user_login(users.user_jgdjy['username'], users.user_jgdjy['pwd'])
         disputepage = DisputePageDjy(self.homepage)
-        disputepage.commit(**jf_info_all)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_commit(**jf_info_all)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_02(self):
-        '''机构登记员-登记纠纷提交-申自然人-被法人代理人'''
-        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申自然人-被法人代理人",
+        '''机构登记员-登记纠纷提交-申自然人-被法人'''
+        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申自然人-被法人",
                        "applicant_type": u"自然人",  # 自然人 法人 非法人组织
                        "disputer_type": u"法人",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
-                       "agent_b_type": "special",  # common special,
+                       "agent_b_type": "",  # common special,
 
                        "jf_appeal": u"假一赔十",
-                       "applicant_name": u"企业或机构名称",  #
+                       "applicant_name": u"a企业或机构名称",  #
                        "world_credit_id": "abcde1234567890",
                        "applicant": u"钱桂林",
                        "applicant_tel": "13160077223",
@@ -81,7 +82,7 @@ class OdrJfInput(unittest.TestCase):
                        "disputer": u"王发明",
                        "disputer_tel": "13913031374",
                        "disputer_world_credit_id": "zxcvbnmasdfghjk123",
-                       "disputer_name": u"企业或机构名称",
+                       "disputer_name": u"b企业或机构名称",
                        "disputer_id": "",
                        "disputer_addr": u"10栋1单元101",
 
@@ -95,21 +96,21 @@ class OdrJfInput(unittest.TestCase):
                        }
         self.homepage.organization_user_login(users.user_jgdjy['username'], users.user_jgdjy['pwd'])
         disputepage = DisputePageDjy(self.homepage)
-        disputepage.commit(**jf_info_all)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_commit(**jf_info_all)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_03(self):
-        '''机构登记员-登记纠纷提交-申自然人-被非法人组织代理人'''
-        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申自然人-被非法人组织代理人",
+        '''机构登记员-登记纠纷提交-申自然人-被非法人组织'''
+        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申自然人-被非法人组织",
                        "applicant_type": u"自然人",  # 自然人 法人 非法人组织
                        "disputer_type": u"非法人组织",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
-                       "agent_b_type": "special",  # common special,
+                       "agent_b_type": "",  # common special,
 
                        "jf_appeal": u"假一赔十",
-                       "applicant_name": u"企业或机构名称",  #
+                       "applicant_name": u"a企业或机构名称",  #
                        "world_credit_id": "abcde1234567890",
                        "applicant": u"钱桂林",
                        "applicant_tel": "13160077223",
@@ -119,7 +120,7 @@ class OdrJfInput(unittest.TestCase):
                        "disputer": u"王发明",
                        "disputer_tel": "13913031374",
                        "disputer_world_credit_id": "zxcvbnmasdfghjk123",
-                       "disputer_name": u"企业或机构名称",
+                       "disputer_name": u"b企业或机构名称",
                        "disputer_id": "",
                        "disputer_addr": u"10栋1单元101",
 
@@ -133,21 +134,21 @@ class OdrJfInput(unittest.TestCase):
                        }
         self.homepage.organization_user_login(users.user_jgdjy['username'], users.user_jgdjy['pwd'])
         disputepage = DisputePageDjy(self.homepage)
-        disputepage.commit(**jf_info_all)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_commit(**jf_info_all)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_04(self):
-        '''机构登记员-登记纠纷提交-申法人-被自然人代理人'''
-        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申法人-被自然人代理人",
+        '''机构登记员-登记纠纷提交-申法人-被自然人'''
+        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申法人-被自然人",
                        "applicant_type": u"法人",  # 自然人 法人 非法人组织
                        "disputer_type": u"自然人",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
-                       "agent_b_type": "special",  # common special,
+                       "agent_b_type": "",  # common special,
 
                        "jf_appeal": u"假一赔十",
-                       "applicant_name": u"企业或机构名称",  #
+                       "applicant_name": u"a企业或机构名称",  #
                        "world_credit_id": "abcde1234567890",
                        "applicant": u"钱桂林",
                        "applicant_tel": "13160077223",
@@ -157,7 +158,7 @@ class OdrJfInput(unittest.TestCase):
                        "disputer": u"王发明",
                        "disputer_tel": "13913031374",
                        "disputer_world_credit_id": "zxcvbnmasdfghjk123",
-                       "disputer_name": u"企业或机构名称",
+                       "disputer_name": u"b企业或机构名称",
                        "disputer_id": "",
                        "disputer_addr": u"10栋1单元101",
 
@@ -171,21 +172,21 @@ class OdrJfInput(unittest.TestCase):
                        }
         self.homepage.organization_user_login(users.user_jgdjy['username'], users.user_jgdjy['pwd'])
         disputepage = DisputePageDjy(self.homepage)
-        disputepage.commit(**jf_info_all)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_commit(**jf_info_all)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_05(self):
-        '''机构登记员-登记纠纷提交-申法人-被法人代理人'''
-        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申法人-被法人代理人",
+        '''机构登记员-登记纠纷提交-申法人-被法人'''
+        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申法人-被法人",
                        "applicant_type": u"法人",  # 自然人 法人 非法人组织
                        "disputer_type": u"法人",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
-                       "agent_b_type": "special",  # common special,
+                       "agent_b_type": "",  # common special,
 
                        "jf_appeal": u"假一赔十",
-                       "applicant_name": u"企业或机构名称",  #
+                       "applicant_name": u"a企业或机构名称",  #
                        "world_credit_id": "abcde1234567890",
                        "applicant": u"钱桂林",
                        "applicant_tel": "13160077223",
@@ -195,7 +196,7 @@ class OdrJfInput(unittest.TestCase):
                        "disputer": u"王发明",
                        "disputer_tel": "13913031374",
                        "disputer_world_credit_id": "zxcvbnmasdfghjk123",
-                       "disputer_name": u"企业或机构名称",
+                       "disputer_name": u"b企业或机构名称",
                        "disputer_id": "",
                        "disputer_addr": u"10栋1单元101",
 
@@ -209,21 +210,21 @@ class OdrJfInput(unittest.TestCase):
                        }
         self.homepage.organization_user_login(users.user_jgdjy['username'], users.user_jgdjy['pwd'])
         disputepage = DisputePageDjy(self.homepage)
-        disputepage.commit(**jf_info_all)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_commit(**jf_info_all)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_06(self):
-        '''机构登记员-登记纠纷提交-申法人-被非法人组织代理人'''
-        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申法人-被非法人组织代理人",
+        '''机构登记员-登记纠纷提交-申法人-被非法人组织'''
+        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申法人-被非法人组织",
                        "applicant_type": u"法人",  # 自然人 法人 非法人组织
                        "disputer_type": u"非法人组织",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
-                       "agent_b_type": "special",  # common special,
+                       "agent_b_type": "",  # common special,
 
                        "jf_appeal": u"假一赔十",
-                       "applicant_name": u"企业或机构名称",  #
+                       "applicant_name": u"a企业或机构名称",  #
                        "world_credit_id": "abcde1234567890",
                        "applicant": u"钱桂林",
                        "applicant_tel": "13160077223",
@@ -233,7 +234,7 @@ class OdrJfInput(unittest.TestCase):
                        "disputer": u"王发明",
                        "disputer_tel": "13913031374",
                        "disputer_world_credit_id": "zxcvbnmasdfghjk123",
-                       "disputer_name": u"企业或机构名称",
+                       "disputer_name": u"b企业或机构名称",
                        "disputer_id": "",
                        "disputer_addr": u"10栋1单元101",
 
@@ -247,21 +248,21 @@ class OdrJfInput(unittest.TestCase):
                        }
         self.homepage.organization_user_login(users.user_jgdjy['username'], users.user_jgdjy['pwd'])
         disputepage = DisputePageDjy(self.homepage)
-        disputepage.commit(**jf_info_all)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_commit(**jf_info_all)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_07(self):
-        '''机构登记员-登记纠纷提交-申非法人组织-被自然人代理人'''
-        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申非法人组织-被自然人代理人",
+        '''机构登记员-登记纠纷提交-申非法人组织-被自然人'''
+        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申非法人组织-被自然人",
                        "applicant_type": u"非法人组织",  # 自然人 法人 非法人组织
                        "disputer_type": u"自然人",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
-                       "agent_b_type": "special",  # common special,
+                       "agent_b_type": "",  # common special,
 
                        "jf_appeal": u"假一赔十",
-                       "applicant_name": u"企业或机构名称",  #
+                       "applicant_name": u"a企业或机构名称",  #
                        "world_credit_id": "abcde1234567890",
                        "applicant": u"钱桂林",
                        "applicant_tel": "13160077223",
@@ -271,7 +272,7 @@ class OdrJfInput(unittest.TestCase):
                        "disputer": u"王发明",
                        "disputer_tel": "13913031374",
                        "disputer_world_credit_id": "zxcvbnmasdfghjk123",
-                       "disputer_name": u"企业或机构名称",
+                       "disputer_name": u"b企业或机构名称",
                        "disputer_id": "",
                        "disputer_addr": u"10栋1单元101",
 
@@ -285,21 +286,21 @@ class OdrJfInput(unittest.TestCase):
                        }
         self.homepage.organization_user_login(users.user_jgdjy['username'], users.user_jgdjy['pwd'])
         disputepage = DisputePageDjy(self.homepage)
-        disputepage.commit(**jf_info_all)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_commit(**jf_info_all)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_08(self):
-        '''机构登记员-登记纠纷提交-申非法人组织-被法人代理人'''
-        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申非法人组织-被法人代理人",
+        '''机构登记员-登记纠纷提交-申非法人组织-被法人'''
+        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申非法人组织-被法人",
                        "applicant_type": u"非法人组织",  # 自然人 法人 非法人组织
                        "disputer_type": u"法人",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
-                       "agent_b_type": "common",  # common special,
+                       "agent_b_type": "",  # common special,
 
                        "jf_appeal": u"假一赔十",
-                       "applicant_name": u"企业或机构名称",  #
+                       "applicant_name": u"a企业或机构名称",  #
                        "world_credit_id": "abcde1234567890",
                        "applicant": u"钱桂林",
                        "applicant_tel": "13160077223",
@@ -309,7 +310,7 @@ class OdrJfInput(unittest.TestCase):
                        "disputer": u"王发明",
                        "disputer_tel": "13913031374",
                        "disputer_world_credit_id": "zxcvbnmasdfghjk123",
-                       "disputer_name": u"企业或机构名称",
+                       "disputer_name": u"b企业或机构名称",
                        "disputer_id": "",
                        "disputer_addr": u"10栋1单元101",
 
@@ -323,21 +324,21 @@ class OdrJfInput(unittest.TestCase):
                        }
         self.homepage.organization_user_login(users.user_jgdjy['username'], users.user_jgdjy['pwd'])
         disputepage = DisputePageDjy(self.homepage)
-        disputepage.commit(**jf_info_all)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_commit(**jf_info_all)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_09(self):
-        '''机构登记员-登记纠纷提交-申非法人组织-被非法人组织代理人'''
-        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申非法人组织-被非法人组织代理人",
+        '''机构登记员-登记纠纷提交-申非法人组织-被非法人组织'''
+        jf_info_all = {"jf_desc": u"机构登记员-登记纠纷提交-申非法人组织-被非法人组织",
                        "applicant_type": u"非法人组织",  # 自然人 法人 非法人组织
                        "disputer_type": u"非法人组织",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
-                       "agent_b_type": "special",  # common special,
+                       "agent_b_type": "",  # common special,
 
                        "jf_appeal": u"假一赔十",
-                       "applicant_name": u"企业或机构名称",  #
+                       "applicant_name": u"a企业或机构名称",  #
                        "world_credit_id": "abcde1234567890",
                        "applicant": u"钱桂林",
                        "applicant_tel": "13160077223",
@@ -347,7 +348,7 @@ class OdrJfInput(unittest.TestCase):
                        "disputer": u"王发明",
                        "disputer_tel": "13913031374",
                        "disputer_world_credit_id": "zxcvbnmasdfghjk123",
-                       "disputer_name": u"企业或机构名称",
+                       "disputer_name": u"b企业或机构名称",
                        "disputer_id": "",
                        "disputer_addr": u"10栋1单元101",
 
@@ -361,9 +362,9 @@ class OdrJfInput(unittest.TestCase):
                        }
         self.homepage.organization_user_login(users.user_jgdjy['username'], users.user_jgdjy['pwd'])
         disputepage = DisputePageDjy(self.homepage)
-        disputepage.commit(**jf_info_all)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_commit(**jf_info_all)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
 
