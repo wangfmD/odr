7dc2d1a73c119404c2651092d594e3f165e4bd64
wangfm <wangfm@3bu.cn>
2018-10-13 18:41:51 +0800

    添加办案法官基本功能用例

--- odrweb/t_wfm/E_jude_funcation/test_funcion_sta.py
+++ odrweb/t_wfm/E_jude_funcation/test_funcion_sta.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-import unittest
 import sys
 import unittest
 
-from odrweb.page.homepage import HomePage
 from odrweb.core.initdata import users
+from odrweb.page.homepage import HomePage
 from odrweb.page.judgepage import JudgePage
 
 reload(sys)
@@ -49,4 +48,30 @@ class JugeFunc(unittest.TestCase):
         result = judgepage.verfc_act_search_select(select_status)
         self.assertEqual(result, True)
 
+    def test_04(self):
+        '''输入查询-案件编号全匹配'''
+        self.homepage.mediator_login(users.user_bafg['username'], users.user_bafg['pwd'])
+        judgepage = JudgePage(self.homepage)
+        # 拿测试数据-查询案件id
+        search_ctx = judgepage._get_case_id()
+        judgepage.act_seacrh_input(search_ctx)
+        result = judgepage.verfc_act_seacrh_input(search_ctx)
+        self.assertEqual(result, True)
 
+    def test_05(self):
+        '''进入案件详情'''
+        self.homepage.mediator_login(users.user_bafg['username'], users.user_bafg['pwd'])
+        judgepage = JudgePage(self.homepage)
+        judgepage.act_goto_case_detail()
+
+    def test_06(self):
+        '''案件详情-返回列表'''
+        self.homepage.mediator_login(users.user_bafg['username'], users.user_bafg['pwd'])
+        judgepage = JudgePage(self.homepage)
+        judgepage.act_goto_case_detail_back()
+
+    def test_07(self):
+        '''法官个人信息修改'''
+        self.homepage.mediator_login(users.user_bafg['username'], users.user_bafg['pwd'])
+        judgepage = JudgePage(self.homepage)
+        judgepage.act_account_save()
