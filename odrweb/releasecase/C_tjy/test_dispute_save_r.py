45d0879f94ba92d00cfde74f4739778af4bd1384
王发明 <wangfm@3bu.cn>
2018-10-15 20:32:16 +0800

    调整用例编排，执行顺序构造测试数据

--- odrweb/releasecase/C_tjy/test_dispute_save_r.py
+++ odrweb/releasecase/C_tjy/test_dispute_save_r.py
@@ -4,6 +4,7 @@ import unittest
 from time import sleep
 
 from odrweb.core.initdata import users
+from odrweb.page.caselistpage import CaseListPage
 from odrweb.page.disputepage import DisputePageTjy
 from odrweb.page.homepage import HomePage
 
@@ -49,140 +50,152 @@ class DisputeSave(unittest.TestCase):
         self.homepage.quit()
 
     def test_01(self):
-        """调解员-登记纠纷提交-申自然人-被自然人"""
-        jf_info = {"jf_desc": u"调解员-登记纠纷提交-申自然人-被自然人",
-                       "applicant_type": u"自然人",  # 自然人 法人 非法人组织
-                       "disputer_type": u"自然人",  # 自然人 法人 非法人组织
-                       "agent_type": "",  # "" common special,
-                       "agent_b_type": "",  # common special,
+        """调解员-登记纠纷保存-申自然人-被自然人"""
 
-                       }
+        jf_info = {"jf_desc": u"调解员-登记纠纷保存-申自然人代理人-被自然人",
+                   "applicant_type": u"自然人",  # 自然人 法人 非法人组织
+                   "disputer_type": u"自然人",  # 自然人 法人 非法人组织
+                   "agent_type": "special",  # "" common special,
+                   "agent_b_type": "",  # common special,
+
+                   }
+        jf_info_all.update(jf_info)
         self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
         disputepage = DisputePageTjy(self.homepage)
-        disputepage.save(**jf_info)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_save(**jf_info)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
+
     def test_02(self):
-        """调解员-登记纠纷提交-申自然人-被法人"""
-        jf_info = {"jf_desc": u"调解员-登记纠纷提交-申自然人-被法人",
+        """调解员-登记纠纷保存-申自然人-被法人"""
+        jf_info = {"jf_desc": u"调解员-登记纠纷保存-申自然人-被法人",
                        "applicant_type": u"自然人",  # 自然人 法人 非法人组织
                        "disputer_type": u"法人",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
                        "agent_b_type": "",  # common special,
                        }
+        jf_info_all.update(jf_info)
         self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
         disputepage = DisputePageTjy(self.homepage)
-        disputepage.save(**jf_info)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_save(**jf_info)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
+
     def test_03(self):
-        """调解员-登记纠纷提交-申自然人-被非法人组织"""
-        jf_info = {"jf_desc": u"调解员-登记纠纷提交-申自然人-被非法人组织",
+        """调解员-登记纠纷保存-申自然人-被非法人组织"""
+        jf_info = {"jf_desc": u"调解员-登记纠纷保存-申自然人-被非法人组织",
                        "applicant_type": u"自然人",  # 自然人 法人 非法人组织
                        "disputer_type": u"非法人组织",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
                        "agent_b_type": "",  # common special,
                        }
+        jf_info_all.update(jf_info)
         self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
         disputepage = DisputePageTjy(self.homepage)
-        disputepage.save(**jf_info)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_save(**jf_info)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_04(self):
-        """调解员-登记纠纷提交-申法人-被自然人"""
-        jf_info = {"jf_desc": u"调解员-登记纠纷提交-申法人-被自然人",
+        """调解员-登记纠纷保存-申法人-被自然人"""
+        jf_info = {"jf_desc": u"调解员-登记纠纷保存-申法人-被自然人",
                        "applicant_type": u"法人",  # 自然人 法人 非法人组织
                        "disputer_type": u"自然人",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
                        "agent_b_type": "",  # common special,
                        }
+        jf_info_all.update(jf_info)
         self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
         disputepage = DisputePageTjy(self.homepage)
-        disputepage.save(**jf_info)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_save(**jf_info)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_05(self):
-        """调解员-登记纠纷提交-申法人-被法人"""
-        jf_info = {"jf_desc": u"调解员-登记纠纷提交-申法人-被法人",
+        """调解员-登记纠纷保存-申法人-被法人"""
+        jf_info = {"jf_desc": u"调解员-登记纠纷保存-申法人-被法人",
                        "applicant_type": u"法人",  # 自然人 法人 非法人组织
                        "disputer_type": u"法人",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
                        "agent_b_type": "",  # common special,
                        }
+        jf_info_all.update(jf_info)
         self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
         disputepage = DisputePageTjy(self.homepage)
-        disputepage.save(**jf_info)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_save(**jf_info)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_06(self):
-        """调解员-登记纠纷提交-申法人-被非法人组织"""
-        jf_info = {"jf_desc": u"调解员-登记纠纷提交-申法人-被非法人组织",
+        """调解员-登记纠纷保存-申法人-被非法人组织"""
+        jf_info = {"jf_desc": u"调解员-登记纠纷保存-申法人-被非法人组织",
                        "applicant_type": u"法人",  # 自然人 法人 非法人组织
                        "disputer_type": u"非法人组织",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
                        "agent_b_type": "",  # common special,
                        }
+        jf_info_all.update(jf_info)
         self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
         disputepage = DisputePageTjy(self.homepage)
-        disputepage.save(**jf_info)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_save(**jf_info)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_07(self):
-        """调解员-登记纠纷提交-申非法人组织-被自然人"""
-        jf_info = {"jf_desc": u"调解员-登记纠纷提交-申非法人组织-被自然人",
+        """调解员-登记纠纷保存-申非法人组织-被自然人"""
+        jf_info = {"jf_desc": u"调解员-登记纠纷保存-申非法人组织-被自然人",
                        "applicant_type": u"非法人组织",  # 自然人 法人 非法人组织
                        "disputer_type": u"自然人",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
                        "agent_b_type": "",  # common special,
 
                        }
+        jf_info_all.update(jf_info)
         self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
         disputepage = DisputePageTjy(self.homepage)
-        disputepage.save(**jf_info)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_save(**jf_info)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_08(self):
-        """调解员-登记纠纷提交-申非法人组织-被法人"""
-        jf_info = {"jf_desc": u"调解员-登记纠纷提交-申非法人组织-被法人",
+        """调解员-登记纠纷保存-申非法人组织-被法人"""
+        jf_info = {"jf_desc": u"调解员-登记纠纷保存-申非法人组织-被法人",
                        "applicant_type": u"非法人组织",  # 自然人 法人 非法人组织
                        "disputer_type": u"法人",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
                        "agent_b_type": "",  # common special,
                        }
+        jf_info_all.update(jf_info)
         self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
         disputepage = DisputePageTjy(self.homepage)
-        disputepage.save(**jf_info)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_save(**jf_info)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
     def test_09(self):
-        """调解员-登记纠纷提交-申非法人组织-被非法人组织"""
-        jf_info = {"jf_desc": u"调解员-登记纠纷提交-申非法人组织-被非法人组织",
+        """调解员-登记纠纷保存-申非法人组织-被非法人组织"""
+        jf_info = {"jf_desc": u"调解员-登记纠纷保存-申非法人组织-被非法人组织",
                        "applicant_type": u"非法人组织",  # 自然人 法人 非法人组织
                        "disputer_type": u"非法人组织",  # 自然人 法人 非法人组织
                        "agent_type": "",  # "" common special,
                        "agent_b_type": "",  # common special,
                        }
+        jf_info_all.update(jf_info)
         self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
         disputepage = DisputePageTjy(self.homepage)
-        disputepage.save(**jf_info)
+        disputepage.save(**jf_info_all)
         sleep(t)
-        res = disputepage.verification_save(**jf_info)
+        res = disputepage.verification_save(**jf_info_all)
         self.assertEqual(True, res)
 
 
--- odrweb/releasecase_check/C_tjy/test_function_list_r.py
+++ odrweb/releasecase_check/C_tjy/test_function_list_r.py
@@ -11,8 +11,7 @@ sys.setdefaultencoding("utf-8")
 
 
 class TjyFunc(unittest.TestCase):
-    """调解员-案件登记列表
-    """
+    """调解员-案件登记列表"""
 
     #
     def setUp(self):
@@ -24,7 +23,7 @@ class TjyFunc(unittest.TestCase):
         self.homepage.quit()
 
     def test_1(self):
-        '''纠纷登记列表-添加纠纷-保存'''
+        """纠纷登记列表-添加纠纷-保存"""
         desc=u"纠纷登记列表-添加纠纷-保存"
         self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
         case_list_page = InputCaseListPage(self.homepage)
@@ -34,7 +33,7 @@ class TjyFunc(unittest.TestCase):
         self.assertEqual(True, result)
 
     def test_2(self):
-        '''纠纷登记列表-添加纠纷-提交'''
+        """纠纷登记列表-添加纠纷-提交"""
         desc=u"纠纷登记列表-添加纠纷-提交"
         self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
         case_list_page = InputCaseListPage(self.homepage)
@@ -43,35 +42,35 @@ class TjyFunc(unittest.TestCase):
         result = case_list_page.verification_add_commit(desc)
         self.assertEqual(True, result)
 
-    def test_3(self):
-        '''纠纷登记列表-纠纷详情-保存'''
-        self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
-        case_list_page = InputCaseListPage(self.homepage)
-        case_list_page.case_modification_save()
-        # 获取返回页面纠纷状态
-        result = case_list_page.verification_dispute_modification()
-        self.assertEqual(True, result)
-
-    def test_4(self):
-        '''纠纷登记列表-删除'''
-        self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
-        case_list_page = InputCaseListPage(self.homepage)
-        case_list_page.dispute_delete()
-        # 获取返回页面纠纷状态
-        # result = case_list_page.verification_dispute_modification()
-        # self.assertEqual(True, result)
-
-    def test_5(self):
-        '''纠纷登记列表-查询-纠纷编号'''
-        self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
-        case_list_page = InputCaseListPage(self.homepage)
-        case_list_page._into_input_case_list()
-        dis_id = case_list_page.get_search_No()
-        #
-        case_list_page.search(dis_id)
-        #
-        result = case_list_page.verification_search_No(dis_id)
-        self.assertEqual(True, result)
+    # def test_3(self):
+    #     """纠纷登记列表-纠纷详情-保存"""
+    #     self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
+    #     case_list_page = InputCaseListPage(self.homepage)
+    #     case_list_page.case_modification_save()
+    #     # 获取返回页面纠纷状态
+    #     result = case_list_page.verification_dispute_modification()
+    #     self.assertEqual(True, result)
+    #
+    # def test_4(self):
+    #     """纠纷登记列表-删除"""
+    #     self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
+    #     case_list_page = InputCaseListPage(self.homepage)
+    #     case_list_page.dispute_delete()
+    #     # 获取返回页面纠纷状态
+    #     # result = case_list_page.verification_dispute_modification()
+    #     # self.assertEqual(True, result)
+    #
+    # def test_5(self):
+    #     """纠纷登记列表-查询-纠纷编号"""
+    #     self.homepage.mediator_login(users.user_tjy['username'], users.user_tjy['pwd'])
+    #     case_list_page = InputCaseListPage(self.homepage)
+    #     case_list_page._into_input_case_list()
+    #     dis_id = case_list_page.get_search_No()
+    #     #
+    #     case_list_page.search(dis_id)
+    #     #
+    #     result = case_list_page.verification_search_No(dis_id)
+    #     self.assertEqual(True, result)
 
 if __name__ == '__main__':
     unittest.main()
